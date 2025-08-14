d1f51592f3f38e849ecc459ea30f0999d459aefe
limingsong <2276695595@qq.com>
2025-08-14 17:14:03 +0800

    feat: 新增测试评估脚本

--- hardware/device.py
+++ hardware/device.py
@@ -10,6 +10,10 @@ def get_device(force_cpu):
     if torch.cuda.is_available() and not force_cpu:
         logging.info("CUDA detected. Running with GPU acceleration.")
         device = torch.device("cuda")
+        # 打印GPU信息 包括型号、内存、计算能力
+        logging.info("GPU Info: {}".format(torch.cuda.get_device_name(0)))
+        logging.info("GPU Memory: {}MB".format(torch.cuda.get_device_properties(0).total_memory / 1024 ** 2))
+        logging.info("GPU Compute Capability: {}".format(torch.cuda.get_device_properties(0).major))
     elif force_cpu:
         logging.info("CUDA detected, but overriding with option '--cpu'. Running with only CPU.")
         device = torch.device("cpu")

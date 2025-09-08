21e0ca9c42c91b1c54aaca26ea1b51e6e0edd633
limingsong <2276695595@qq.com>
2025-09-08 11:17:16 +0800

    feat: 优化训练脚本

--- train_network.py
+++ train_network.py
@@ -512,6 +512,23 @@ if __name__ == '__main__':
     # 7. goa + aff
     # python train_network.py --network grconvnet_goa --dataset cornell --dataset-path D:\\datasets\\cornell_grasp --description training_cornell --use-dropout 1 --input-size 300 --split 0.8 --goa 1 --aff 1
 
+    # jacquard unet
+    # python train_network.py --network grconvnet_goa --dataset jacquard --dataset-path D:\\datasets\\Jacquard --description training_Jacquard --use-dropout 1 --input-size 300 --split 0.9 --unet 1
+
     # jacquard goa
     # python train_network.py --network grconvnet_goa --dataset jacquard --dataset-path D:\\datasets\\Jacquard --description training_Jacquard --use-dropout 1 --input-size 300 --split 0.9 --goa 1
+
+    # jacquard unet+goa
+    # python train_network.py --network grconvnet_goa --dataset jacquard --dataset-path D:\\datasets\\Jacquard --description training_Jacquard --use-dropout 1 --input-size 300 --split 0.9 --unet 1 --goa 1
+
+    # jacquard unet+aff
+    # python train_network.py --network grconvnet_goa --dataset jacquard --dataset-path D:\\datasets\\Jacquard --description training_Jacquard --use-dropout 1 --input-size 300 --split 0.9 --unet 1 --aff 1
+
+    # jacquard unet+goa+aff
+    # python train_network.py --network grconvnet_goa --dataset jacquard --dataset-path D:\\datasets\\Jacquard --description training_Jacquard --use-dropout 1 --input-size 300 --split 0.9 --unet 1 --goa 1 --aff 1
+
+    # jacquard FPN + GOA + AFF
+    # python train_network.py --network grconvnet_goa --dataset jacquard --dataset-path D:\\datasets\\Jacquard --description training_Jacquard --use-dropout 1 --input-size 300 --split 0.9 --fpn 1 --goa 1 --aff 1
+
+
     run()
